146f51700969731c0ae8500ea3f3794a8b57be73
MiguelMSandin <miguelmendezsandin@gmail.com>
2021-09-18 13:07:37 +0200

    Update 1.2_blastnClean.py

--- scripts/1.2_blastnClean.py
+++ scripts/1.2_blastnClean.py
@@ -15,39 +15,37 @@ requiredArgs.add_argument("-o", "--output", dest="file_out", required=True,
 
 args = parser.parse_args()
 
-print("")
-print("  Setting hit names")
+print("Cleaning")
+
 clean = {}
+hits = set()
+lines = 0
 removed = 0
-accepted = 0
 selfHit = 0
-for line in open(args.file_in):
-    line = line.strip().split()
-    seq1 = line[0]
-    seq2 = line[1]
-    
-    if seq1 == seq2:
-        selfHit = selfHit + 1
-    
-    hit = str(seq1) + "\t" + str(seq2)
-    hitr =str(seq2) + "\t" + str(seq1)
-    
-    if hit in clean:
-        removed = removed + 1        
-    elif hitr in clean:
-        removed = removed + 1   
-    else:        
-        values = str(line[2]) + "\t" + str(line[3]) + "\t" + str(line[4]) + "\t" + str(line[5]) + "\t" + str(line[6]) + "\t" + str(line[7]) + "\t" + str(line[8]) + "\t" + str(line[9]) + "\t" + str(line[10])
-        
-        clean[hit] = values
-        accepted = accepted + 1
-
-print("  Writing cleaned file with '", accepted, "' hits", sep="")
+accepted = 0
 with open(args.file_out, "w") as outfile:
-    for hit in list(clean.keys()):
-        print(hit + "\t" + clean[hit], file=outfile)
+	for line in open(args.file_in):
+		lines += 1
+		line = line.strip().split()
+		seq1 = line[0]
+		seq2 = line[1]
+		if seq1 == seq2:
+			selfHit += 1
+		hit = str(seq1) + "\t" + str(seq2)
+		hitr =str(seq2) + "\t" + str(seq1)
+		if hit in clean or hitr in clean:
+			removed += 1
+		else:
+			accepted += 1
+			hits.add(hit)
+			hits.add(hitr)
+			values = str(line[2]) + "\t" + str(line[3]) + "\t" + str(line[4]) + "\t" + str(line[5]) + "\t" + str(line[6]) + "\t" + str(line[7]) + "\t" + str(line[8]) + "\t" + str(line[9]) + "\t" + str(line[10])
+			clean[hit] = values
+			print(hit + "\t" + clean[hit], file=outfile)
 
-print("    Removed hits:\t", removed)
-print("    Self matching:\t", selfHit)
+print("  Hits in:      ", lines)
+print("  Hits removed: ", removed)
+print("  Self hits:    ", selfHit)
+print("  Hits out:     ", accepted)
 print("Done")
-print("")
+
